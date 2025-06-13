02e1d0932e7bbe8a0662eb24ce6afbaf0a89905c
innovation <wangshigexin311@outlook.com>
2025-06-13 18:20:38 +0800

    1.1.5

---
+++ MDK-ARM/Math.h
@@ -0,0 +1,6 @@
+#ifndef __MATH2_H__
+#define __MATH2_H__
+
+#include "arm_math.h"
+
+#endif 
---
+++ MDK-ARM/MoterDevice2.0.0/bayesfunctionsf16.d
@@ -0,0 +1,16 @@
+moterdevice2.0.0/bayesfunctionsf16.o: \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Source\BayesFunctions\BayesFunctionsF16.c \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Source\BayesFunctions\arm_gaussian_naive_bayes_predict_f16.c \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\dsp\bayes_functions_f16.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\arm_math_types_f16.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\arm_math_types.h \
+  ..\Drivers\CMSIS\Include\cmsis_compiler.h \
+  ..\Drivers\CMSIS\Include\cmsis_armclang.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\arm_math_memory.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\dsp\none.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\dsp\utils.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\dsp\statistics_functions_f16.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\dsp\basic_math_functions_f16.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\dsp\fast_math_functions_f16.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\dsp\fast_math_functions.h \
+  D:\keil\pack\ARM\CMSIS-DSP\1.16.2\Include\dsp\basic_math_functions.h
